eb6554e2c14416fe893904b486058a08dc03f0a1
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-08 16:29:49 +0900

    訂正しました。よろしくお願いします。

--- Question4_7/src/Question4_7/Question4_7.java
+++ Question4_7/src/Question4_7/Question4_7.java
@@ -2,44 +2,51 @@ package Question4_7;
 
 import java.util.Scanner;
 /*
- * クラス名			:ensyu4_7
- * 概要    			:読み込んだ値の個数だけ記号を入力するプログラム
- * 作成者 			:中西輝
- * 作成日  			:2026.4.7
- * 関数名           :main
- * メソッドの説明   :～～
- * パラメータの説明 :なし
- * 返り値           :なし
+ * クラス名            :ensyu4_7
+ * 概要                :読み込んだ値の個数だけ記号を入力するプログラム
+ * 作成者             :中西輝
+ * 作成日              :2026.4.7
  */
+
 // Question4_7のクラス
 public class Question4_7 {
-	//関数ヘッダ
+	  /*
+     * 関数名           :main
+     * メソッドの説明   :～～
+     * パラメータの説明 :なし
+     * 返り値           :なし
+     * 作成者             :中西輝
+     * 作成日              :2026.4.7
+     */
 	public static void main(String[] args) {
-		//キーボードで入力
+		//キーボードに入力された値を読み込むための変数を作る
 		Scanner standardInput = new Scanner(System.in);
 		//何個表示しますかを表示
 		System.out.print("何個表示しますか:");
-		//firstvalueに値を入力
-		int firstvalue = standardInput.nextInt();
-		//変数secondvalueにfirstvalueを代入
-		int secondvalue = firstvalue ;
+		//ユーザーが入力した値を変数に代入
+		int inputValue = standardInput.nextInt();
+		//変数iにfirstvalueを代入
+		int i = inputValue ;
 		
 		//繰り返し
 		do {
-			//secondvalueが奇数の場合
-			if(secondvalue%2==1) {
+			if(i == 0) {
+				System.out.print("");
+			}
+			//iが奇数の場合
+			else if(i % 2 == 1) {
 				//*を表示する
 				System.out.print("*");
 			}
-			//secondvalueが偶数の場合
-			 else if (secondvalue%2==0){
+			//iが偶数の場合
+			 else if (i % 2 == 0){
 				//+を表示する
 				System.out.print("+");
 			}
 			//secondvalue-1
-			secondvalue--;
-		//secondvalueが0より大きい場合は繰り返す
-		}while(secondvalue>0);
+			i--;
+		//iが0より大きい場合は繰り返す
+		}while(i > 0);
 		
 
 	}
