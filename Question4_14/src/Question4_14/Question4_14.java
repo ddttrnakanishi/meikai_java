fde4e82e0a658b2edc1978d60c4fbdae1f49152e
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-09 18:17:11 +0900

    修正しました

--- Question4_14/src/Question4_14/Question4_14.java
+++ Question4_14/src/Question4_14/Question4_14.java
@@ -19,30 +19,30 @@ public class Question4_14 {
 
 	public static void main(String[] args) {
 		//キーボードで入力する値を読み込む変数を作る
-				Scanner standardInput = new Scanner(System.in);
-				//変数inputNumberの初期化
-				int inputNumber = 0;
-				//繰り返し
-				do {
-					//inputNumberの値:を表示
-					System.out.print("inputNumberの値:");
-					//変数inputNumberに入力した値を代入
-					inputNumber = standardInput.nextInt();
-					//inputNumberが0以上になるまで繰り返す
-				}while(inputNumber < 0);
-				//変数sumNumberの初期化
-				int sumNumber = 0;
-				//変数iを1から始めて変数iを1ずつ増やしていきinputNumber回繰り返す
-				for(int i = 1;i <= inputNumber; i ++)
-					//sumNumberにiを足す
-					sumNumber+= i;
-				//変数iを1から始めて変数iを1ずつ増やしていきinputValue回繰り返す
-				for(int i = 1; i <= inputNumber ;i++)
-					//変数iの値と+を表示
-					System.out.print(i+"+");
-				//=と変数sumNumberを表示
-				System.out.println("="+sumNumber);
-				
+		Scanner standardInput = new Scanner(System.in);
+		//変数inputNumberの初期化
+		int inputNumber = 0;
+		//繰り返し
+		do {
+			//inputNumberの値:を表示
+			System.out.print("inputNumberの値:");
+			//変数inputNumberに入力した値を代入
+			inputNumber = standardInput.nextInt();
+			//inputNumberが0以上になるまで繰り返す
+		}while(inputNumber < 0);
+		//変数sumNumberの初期化
+		int sumNumber = 0;
+		//変数iを1から始めて変数iを1ずつ増やしていきinputNumber回繰り返す
+		for(int i = 1;i <= inputNumber; i ++)
+			//sumNumberにiを足す
+			sumNumber += i;
+		//変数iを1から始めて変数iを1ずつ増やしていきinputNumber回繰り返す
+		for(int i = 1; i <= inputNumber ;i++)
+			//変数iの値と+を表示
+			System.out.print(i+"+");
+		//=と変数sumNumberを表示
+		System.out.println("="+ sumNumber);
+
 
 	}
 
