e3b61a034ec686dfbb2ca12c882c747a2731f209
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-10 16:22:28 +0900

    演習4-16,17終わりました

---
+++ Question4_16/src/Question4_16/Question4_16.java
@@ -0,0 +1,52 @@
+package Question4_16;
+
+import java.util.Scanner;
+/*
+ * クラス名            :ensyu4_16
+ * 概要                :読み込んだだけ*を表示するプログラム
+ * 作成者             :中西輝
+ * 作成日              :2026.4.10
+ */
+
+public class Question4_16 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :～～
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者             :中西輝
+	 * 作成日              :2026.4.10
+	 */
+
+	public static void main(String[] args) {
+		//キーボードで入力した値を読み込む変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//変数displayNumberの初期化
+		int displayNumber = 0;
+		//繰り返す
+		do {
+		//何個*を表示しますか:を表示
+		System.out.print("何個*を表示しますか:");
+		//変数displayNumberに値を入力する
+		displayNumber = standardInput.nextInt();
+		//0未満の時繰り返す
+	}while(displayNumber < 0);
+		//1未満の時
+		if (displayNumber < 1) {
+			//何も表示しない
+			System.out.println();
+		}
+		//変数iを1から始めて変数iを1ずつ増やしていきdisplayNumber回繰り返す
+		for(int i = 1 ; i <= displayNumber ;i++) {
+			//*を表示
+			System.out.print("*");
+			//変数i割る5の余りが0の場合
+			if (i % 5 == 0){
+			//改行する
+			System.out.println()
+;			}
+		}
+			
+
+}
+}
---
+++ Question4_17/src/Question4_17/Question4_17.java
@@ -0,0 +1,51 @@
+package Question4_17;
+
+import java.util.Scanner;
+/*
+ * クラス名            :ensyu4_17
+ * 概要                :読み込んだ整数値のすべての約数を表示
+ * 作成者             :中西輝
+ * 作成日              :2026.4.10
+ */
+
+public class Question4_17 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :～～
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者             :中西輝
+	 * 作成日              :2026.4.10
+	 */
+
+	public static void main(String[] args) {
+		//キーボードで入力した値を読み込む変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//変数firstNumberの初期化
+		int firstNumber = 0;
+		//繰り返し
+		do {
+			//整数値を表示
+			System.out.print("整数値:");
+			//変数firstNumberに入力した値を代入
+			firstNumber = standardInput.nextInt();
+		//変数firstNumberが0以下の場合繰り返す
+		}while(firstNumber <= 0);
+		//変数sumNumberの初期化
+		int sumNumber = 0;
+		//変数iが1から始まり1ずつ増やしたときfirstNumber回繰り返す
+		for(int i = 1;i <= firstNumber;i++) {
+			//変数firstNumberをiで割ったときの余りが0の場合
+			if(firstNumber % i == 0) {
+				//変数iの値を表示
+				System.out.println(i);
+				//変数sumNumberに+1をする
+				sumNumber ++;
+			}
+		}
+		//約数はsumNumber個です。と表示
+		System.out.println("約数は"+sumNumber+"個です。");
+
+	}
+
+}
