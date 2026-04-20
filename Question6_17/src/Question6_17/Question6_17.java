196a45849afcdfbf327660cfecccedda1838829b
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-20 16:30:36 +0900

    終わりました

--- Question6_17/src/Question6_17/Question6_17.java
+++ Question6_17/src/Question6_17/Question6_17.java
@@ -22,7 +22,7 @@ public class Question6_17 {
 		//int型を構成要素型とする配列を構成要素型とする配列の宣言
 		int [][] resultMatrix = new int[2][6];
 		//String型を構成要素型とする配列の宣言
-		String[] SUBJECTSCORE = {"国語","数学"};
+		final String[] SUBJECTSCORE = {"国語","数学"};
 		//変数iを0から始めて1ずつ増やしていき2になるまで繰り返す(科目)
 		for(int i = 0; i < 2; i++) {
 			//変数jを0から始めて1ずつ増やしていき6になるまで繰り返す(人数)
