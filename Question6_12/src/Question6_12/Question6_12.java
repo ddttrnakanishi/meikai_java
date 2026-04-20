bb42ccd6095c6b6262c43ff87a9dcb5ee58ec548
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-20 18:06:33 +0900

    修正しました

--- Question6_12/src/Question6_12/Question6_12.java
+++ Question6_12/src/Question6_12/Question6_12.java
@@ -26,10 +26,8 @@ public class Question6_12 {
 		final int elementNumber = 5;
 		//変数iを初期化
 		int i = 0;
-		//変数jを初期化
-		int j = 0;
 		//int型を構成要素とする配列要素数の作成
-		int[] arrayNumber = {1,2,3,4,5,};
+		int[] arrayNumber = {1,2,3,4,5};
 		////変数1が0から始まり1ずつ増やしていき要素数より大きくなるまで繰り返す
 		for (i = 0; i < arrayNumber.length; i++) {
 			//""の中の文字とiの値とarrayNumber[i]の値を表示して改行
@@ -42,6 +40,8 @@ public class Question6_12 {
 		}	
 		//改行
 		System.out.println();
+		//変数jを初期化
+		int j = 0;
 		//変数shuffleNumber1の初期化
 		int shuffleNumber1 = 0;
 		//変数jが0から始まり1ずつ増やしていき1入力値より大きくなるまで繰り返す
