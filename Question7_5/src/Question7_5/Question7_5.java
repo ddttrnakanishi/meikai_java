906cc69d94548f81425581e47aeede06c97c40a7
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-21 14:51:40 +0900

    終わりました

---
+++ Question7_5/src/Question7_5/Question7_5.java
@@ -0,0 +1,26 @@
+package Question7_5;
+
+/*
+ * クラス名            :Question7_5
+ * 概要                :こんにちは。と表示して改行する
+ * 作成者             　:中西輝
+ * 作成日              :2026.4.21
+ */
+public class Question7_5 {
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :メイン処理とmedianNumber処理
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :中西輝
+	 * 作成日           :2026.4.21
+	 */
+	static  void hello() {
+		//""の中の文字を表示して改行
+		System.out.println("こんにちは。");
+	}
+	public static void main(String[] args) {
+		//helloメソッドの呼び出し
+		hello();
+	}
+}
--- 添削用/src/添削用/添削用.java
+++ 添削用/src/添削用/添削用.java
@@ -7,38 +7,34 @@ public class 添削用 {
 
 	public static void main(String[] args) {
 		//キーボードに入力された値を読み込むための変数を作る
-				Scanner standardinput = new Scanner(System.in);
-				// 行数入力
-				System.out.print("行数を入力してください：");
+				Scanner standardInput = new Scanner(System.in);
+				//「クラス数：」を表示
+				System.out.print("クラス数：");
 				//整数値を読み込む
-				int rowNumber = standardinput.nextInt();
+				int classNumber = standardInput.nextInt();
+				//改行
+				System.out.println();
+				//変数を初期化
+				int totalNumber = 0;
+				//変数を初期化
+				int countNumber = 0;
 				//配列を宣言
-				int[][] c = new int[rowNumber][];
-				// iが行数と値が小さいとき、インクリメントしながら繰り返す
-				for (int i = 0; i < rowNumber; i++) {
-					//列数の入力を表示
-					System.out.print(i + "行目の列数を入力してください：");
-					//整数値を読み込む
-					int colNumber = standardinput.nextInt();
-					//配列を宣言
-					c[i] = new int[colNumber];
-					//iが列数と値が小さいとき、インクリメントしながら繰り返す
-					for (int j = 0; j < colNumber; j++) {
-						//配列の値を表示
-						System.out.print("c[" + i + "][" + j + "] の値：");
-						//整数値を読み込む
-						c[i][j] = standardinput.nextInt();
+				int [][]arrayNumber = new int [1][1];
+				//iが配列の長さよりも小さいとき、インクリメントしながら繰り返す
+				for (int i = 0; i < arrayNumber .length; i++) {
+					//jが配列の長さよりも小さいとき、インクリメントしながら繰り返す
+					for (int j = 0; j < arrayNumber [i].length; j++) {
+						//足す
+						totalNumber += arrayNumber [i][j];
+						//インクリメントする
+						countNumber++;
 					}
 				}
-				//iが配列の長さよりも小さいとき、インクリメントしながら繰り返す
-				for (int i = 0; i < c.length; i++) {
-					//jが配列の長さよりも小さいとき、インクリメントしながら繰り返す
-					for (int j = 0; j < c[i].length; j++) {
-						//表示
-						System.out.printf("%3d", c[i][j]);
-					}
-					//改行
-					System.out.println();
-				}   
+				//平均を表示
+				double aveNumber = (double)totalNumber / countNumber;
+				//合計を表示
+				System.out.println("全体合計：" + totalNumber);
+				//平均を表示
+				System.out.println("全体平均：" + aveNumber);
 			}
 		}
