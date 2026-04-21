9424619863a9461a31c03ee0222b7cc0b6fba1ae
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-21 19:12:52 +0900

    a

---
+++ Question7_8/src/Question7_8/Question7_8.java
@@ -0,0 +1,10 @@
+package Question7_8;
+
+public class Question7_8 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- 添削用/src/添削用/添削用.java
+++ 添削用/src/添削用/添削用.java
@@ -5,36 +5,49 @@ import java.util.Scanner;
 
 public class 添削用 {
 
-	public static void main(String[] args) {
-		//キーボードに入力された値を読み込むための変数を作る
-				Scanner standardInput = new Scanner(System.in);
-				//「クラス数：」を表示
-				System.out.print("クラス数：");
-				//整数値を読み込む
-				int classNumber = standardInput.nextInt();
-				//改行
-				System.out.println();
-				//変数を初期化
-				int totalNumber = 0;
-				//変数を初期化
-				int countNumber = 0;
-				//配列を宣言
-				int [][]arrayNumber = new int [1][1];
-				//iが配列の長さよりも小さいとき、インクリメントしながら繰り返す
-				for (int i = 0; i < arrayNumber .length; i++) {
-					//jが配列の長さよりも小さいとき、インクリメントしながら繰り返す
-					for (int j = 0; j < arrayNumber [i].length; j++) {
-						//足す
-						totalNumber += arrayNumber [i][j];
-						//インクリメントする
-						countNumber++;
-					}
-				}
-				//平均を表示
-				double aveNumber = (double)totalNumber / countNumber;
-				//合計を表示
-				System.out.println("全体合計：" + totalNumber);
-				//平均を表示
-				System.out.println("全体平均：" + aveNumber);
-			}
+	static void printSeason(int choiceMonth) {
+		//choiceMonthが3,4,5のどれかの場合
+		if(choiceMonth == 3 || choiceMonth == 4 || choiceMonth == 5){
+			//"春"を表示して改行する
+			System.out.println("春");
 		}
+		//choiceMonthが6,7,8のどれかの場合
+		else if(choiceMonth == 6 || choiceMonth == 7 || choiceMonth == 8){
+			//"夏"を表示して改行する
+			System.out.println("夏");
+		}
+		//choiceMonthが9,10,11のどれかの場合
+		else if(choiceMonth == 9 || choiceMonth == 10 || choiceMonth == 11){
+			//"秋"を表示して改行する
+			System.out.println("秋");
+		}
+		//choiceMonthが12,1,2のどれかの時
+		else if(choiceMonth == 12 || choiceMonth == 1 || choiceMonth == 2){
+			//"冬"を表示して改行する
+			System.out.println("冬");
+		}
+		//その他の場合
+		else {
+			//改行する
+			System.out.println();
+		}
+	}
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :メイン処理
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.21
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//"何月："を表示する
+		System.out.print("何月？：");
+		//whatMonthに入力した値を代入する
+		int whatMonth = standardInput.nextInt();
+		//printSeasonメソッドの呼び出し
+		printSeason(whatMonth);
+	}
+}
