1664ad44ce2d79c8fab2583cd79b022a170831e1
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-23 18:44:56 +0900

    終わりました

---
+++ Question7_16/src/Question7_16/Question7_16.java
@@ -0,0 +1,62 @@
+package Question7_16;
+
+import java.util.Scanner;
+/*
+ * クラス名             :Question7_16
+ * 概要                 :配列arrayNumberの要素の最小値を求めるメソッドの作成
+ * 作成者             　:中西輝
+ * 作成日               :2026.4.23
+ */
+public class Question7_16 {
+	/*
+	 * 関数名           　:main
+	 * メソッドの説明   　:要素数の入力と全要素の入力、minOfメソッドの結果を表示
+	 * パラメータの説明　 :なし
+	 * 返り値          　 :なし
+	 * 作成者          　 :中西輝
+	 * 作成日          　 :2026.4.23	
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//""の中の文字を表示
+		System.out.print("要素数 :");
+		//要素数を入力
+		int inputNumber = standardInput.nextInt();
+		//配列の作成
+		int[] arrayNumber = new int[inputNumber];
+		//変数iを0から初めて1ずつ増やしていき要素数の価まで繰り返す
+		for(int i = 0; i < inputNumber; i++) {
+			//""の中の文字とiの値を表示
+			System.out.print("arrayNumber [" + i + "] =");
+			//要素の値を代入
+			arrayNumber[i] = standardInput.nextInt();
+		}
+		//""の文字とminOfメソッドの結果を表示して改行
+		System.out.println("最小値は :" + minOf(arrayNumber));
+	}
+	/*
+	 * 関数名           　:minOf
+	 * メソッドの説明   　:要素の中での最小値を求める
+	 * パラメータの説明　 :配列arrayNumber
+	 * 返り値          　 :最小値
+	 * 作成者          　 :中西輝
+	 * 作成日          　 :2026.4.23	
+	 */
+	static int minOf(int[] arrayNumber) {
+		//最小値に[0]の要素の値を代入
+		int minNumber = arrayNumber[0];
+		//変数iが1から始まり1ずつ増やしていき要素の数分繰り返す
+		for(int i = 1; i < arrayNumber.length; i++) {
+			//最小値が要素の価よりも大きいとき
+			if(minNumber > arrayNumber[i]) {
+				//その要素の値を最小値に代入
+				minNumber = arrayNumber[i];
+			}
+		}
+		//返却
+		return minNumber;
+	}
+}
+
+
--- 添削用/src/添削用/添削用.java
+++ 添削用/src/添削用/添削用.java
@@ -4,34 +4,85 @@ import java.util.Scanner;
 
 public class 添削用 {
 
-	public static void main(String[] args) {
-		//「正の整数値：」を表示
-		System.out.print("正の整数値：");
-		//メゾット「readPlusInt」を表示
-		readPlusInt();
+	static void printBits(int inputNumber) {
+		//変数iが31から始めて1ずつ減らしていきiが0まで繰り返す
+		for(int i = 31; i >= 0; i--)
+			//"iビット目が1なら'1'を0なら'0'"を表示する
+			System.out.print(((inputNumber >>> i & 1) == 1)? '1' : '0');
 	}
-	/**
-	 * 関数名           :readPlusInt
-	 * メソッドの説明   :readPlusIntを処理する
-	 * パラメータの説明 :なし
-	 * 返り値           :inputNumber
-	 * 作成者  :田中沙樹
-	 * 作成日  :2026.04.21.
+	/*
+	 * 関数名           :rRotate
+	 * メソッドの説明   :整数を右に回転した値を返却する
+	 * パラメータの説明 :回転前の整数、回転数
+	 * 返り値           :右にビット回転した値
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.23
 	 */
-	static int readPlusInt() {
-		//キーボードから読み込むための変数を作る
+	public static int rRotate(int originalNumber, int shiftNumber) {
+		//もしshiftNumberが0より小さい場合
+		if(shiftNumber < 0)
+			//-shiftNumber（逆回転、左）にして返却する
+			return lRotate(originalNumber, -shiftNumber);
+				/*元の整数をshiftNumberビット右シフトして0を詰めた値と、
+		 *元の整数を32-shiftNumberビット左シフトして0を詰めた値の論理和を返却する
+		 */
+		return (originalNumber >>> shiftNumber) | (originalNumber << (32 - shiftNumber));
+	}
+	/*
+	 * 関数名           :lRotate
+	 * メソッドの説明   :整数を左に回転した値を返却する
+	 * パラメータの説明 :回転前の整数、回転数
+	 * 返り値           :左にビット回転した値
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.23
+	 */
+	public static int lRotate(int originalNumber, int shiftNumber) {
+		//もしshiftNumberが0より小さい場合
+		if (shiftNumber < 0) 
+			//-shiftNumber（逆回転、右）にして返却する
+			return rRotate(originalNumber, -shiftNumber);
+		
+		/*元の整数をshiftNumberビット左シフトして0を詰めた値と、
+		 *元の整数を32-shiftNumberビット右シフトして0を詰めた値の論理和を返却する
+		 */
+		return (originalNumber << shiftNumber) | (originalNumber >>> (32 - shiftNumber));
+	}
+	/*
+	 * 関数名           :main
+	 * メソッドの説明   :値を入力し左右に回転させた結果をビット表示する
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :玉澤一輝
+	 * 作成日           :2026.4.23
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
 		Scanner standardInput = new Scanner(System.in);
-		//整数値を読み込む
-		int inputNumber = standardInput.nextInt();
-		//実行する
-		do {
-			//「正の整数値：」を表示
-			System.out.print("正の整数値：");
-			//再入力
-			inputNumber = standardInput.nextInt();
-			//入力値が0以下の場合、繰り返す
-		} while(inputNumber <= 0);
-		//返却
-		return inputNumber;
+		//"整数x"を表示する
+		System.out.print("整数x：");
+		//変数integerNumberに入力した値を代入する
+		int integerNumber = standardInput.nextInt();
+		//"回転数n"を表示する
+		System.out.print("回転数n：");
+		//変数rotationNumberに入力した値を代入する
+		int rotationNumber = standardInput.nextInt();
+		//"元のビット数　 ："を表示する
+		System.out.print("元のビット数　 ：");
+		//入力された整数のビット構成を表示する
+		printBits(integerNumber);
+		//改行する
+		System.out.println();
+		//"右にnビット回転："を表示する
+		System.out.print("右にnビット回転：");
+		//整数を回転数nだけ右シフトした結果のビット構成を表示する
+		printBits(rRotate(integerNumber, rotationNumber));
+		//改行する
+		System.out.println();
+		//"左にnビット回転："を表示する
+		System.out.print("左にnビット回転：");
+		//整数を回転数nだけ左シフトした結果のビット構成を表示する
+		printBits(lRotate(integerNumber, rotationNumber));
+		//改行する
+		System.out.println();
 	}
 }
