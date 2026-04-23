d9593ce255da13adf8b45138b96925ef6e5eb598
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-23 14:09:20 +0900

    修正しました

--- Question7_13/src/Question7_13/Question7_13.java
+++ Question7_13/src/Question7_13/Question7_13.java
@@ -18,21 +18,21 @@ public class Question7_13 {
 	 */
 	public static void main(String[] args) {
 		//キーボードで入力した値を入れる変数を作る
-				Scanner standardInput = new Scanner(System.in);
-				//""の中の文字を表示して改行
-				System.out.println("整数を入力してください");
-				//""の中の文字を表示
-				System.out.print("integerValue : ");
-				//整数値を入力
-				int integerValue = standardInput.nextInt();
-				//""の中の文字を表示
-				System.out.print("シフトするビット数 :");
-				//シフトする数を代入
-				int shiftNumber = standardInput.nextInt();
-	
-				//返却値を受けとる
-				printBits(integerValue,shiftNumber);
-			}
+		Scanner standardInput = new Scanner(System.in);
+		//""の中の文字を表示して改行
+		System.out.println("整数を入力してください");
+		//""の中の文字を表示
+		System.out.print("integerValue : ");
+		//整数値を入力
+		int integerValue = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("シフトするビット数 :");
+		//シフトする数を代入
+		int shiftNumber = standardInput.nextInt();
+
+		//返却値を受けとる
+		printBits(integerValue,shiftNumber);
+	}
 	/*
 	 * 関数名           　:printBits
 	 * メソッドの説明   　:入力した整数値を32のビット数で表示し右にシフトしたビット
@@ -51,25 +51,25 @@ public class Question7_13 {
 		//改行
 		System.out.println();
 		//変数iを32から始めて1ずつ減らしていき0になるまで繰り返す
-				for(int i = 31; i >= 0; i--) {
-					//rRotate返却値の値が1の時は1をそうでないときは0を表示
-					System.out.print(((set(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
-				}
-				//改行
-				System.out.println();
-				//変数iを32から始めて1ずつ減らしていき0になるまで繰り返す
-				for(int i = 31; i >= 0; i--) {
-					//reset返却値の値が1の時は1をそうでないときは0を表示
-					System.out.print(((reset(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
-				}
-				//改行
-				System.out.println();
-				//変数iを32から始めて1ずつ減らしていき0になるまで繰り返す
-				for(int i = 31; i >= 0; i--) {
-					//inverse返却値の値が1の時は1をそうでないときは0を表示
-					System.out.print(((inverse(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
-				}
-		
+		for(int i = 31; i >= 0; i--) {
+			//rRotate返却値の値が1の時は1をそうでないときは0を表示
+			System.out.print(((set(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
+		}
+		//改行
+		System.out.println();
+		//変数iを32から始めて1ずつ減らしていき0になるまで繰り返す
+		for(int i = 31; i >= 0; i--) {
+			//reset返却値の値が1の時は1をそうでないときは0を表示
+			System.out.print(((reset(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
+		}
+		//改行
+		System.out.println();
+		//変数iを32から始めて1ずつ減らしていき0になるまで繰り返す
+		for(int i = 31; i >= 0; i--) {
+			//inverse返却値の値が1の時は1をそうでないときは0を表示
+			System.out.print(((inverse(integerValue,shiftNumber) >>> i & 1) == 1) ? '1':'0');
+		}
+
 
 	}
 	/*
@@ -85,7 +85,7 @@ public class Question7_13 {
 		int setNumber  = integerValue | (1 << shiftNumber);
 		//返却
 		return setNumber;
-		
+
 	}
 	/*
 	 * 関数名           　:reset
@@ -101,7 +101,7 @@ public class Question7_13 {
 		int setNumber  = integerValue & (integerValue ^ (1 << shiftNumber));
 		//返却
 		return setNumber;
-		
+
 	}
 	/*
 	 * 関数名           　:inverse
