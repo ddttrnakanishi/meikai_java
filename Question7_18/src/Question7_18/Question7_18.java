323d0c8c739c11f5ed0c86f73faba6dfc5a89dc3
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-24 11:16:59 +0900

    終わりました

--- Question7_18/src/Question7_18/Question7_18.java
+++ Question7_18/src/Question7_18/Question7_18.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  */
 public class Question7_18 {	
 	//クラス全体で使える変数FINAL_VALUEに1を代入
-	 final static int FINAL_VALUE = 1;
+	final static int FINAL_VALUE = 1;
 	/*	
 	 *  関数名 　        :main	
 	 *  メソッドの説明 　:要素数、各要素の値、削除する場所を入力しaryRmvの結果を表示する	
@@ -20,36 +20,36 @@ public class Question7_18 {
 	 */
 	public static void main(String[] args) {
 		//キーボードで入力した値を入れる変数を作る
-				Scanner standardInput = new Scanner(System.in);
-				//""の中の文字を表示
-				System.out.print("要素数 :");
-				//要素数を入力
-				int inputNumber = standardInput.nextInt();
-				//入力値の分だけ整数を入れられる配列を作る
-				int[] arrayNumber = new int [inputNumber]; 
-				//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
-				for(int i = 0; i < inputNumber; i++) {
-					//""の中の文字とiの値を表示
-					System.out.print("arrayNumber [" + i + "} =");
-					//配列[i]の値を入力
-					arrayNumber[i] = standardInput.nextInt();
-				}
-				//""の中の文字を表示
-				System.out.print("{");
-				//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
-				for(int i = 0; i < inputNumber - FINAL_VALUE; i++) {
-				//arrayNumber[i]の値と""の中の文字を表示
-				System.out.print(arrayNumber[i] + ", ");	
-				}
-				//arrayNumber[要素数から-1]の値と""の中の文字を表示して改行
-				System.out.println(arrayNumber[inputNumber - FINAL_VALUE] + "}");
-				//""の中の文字を表示
-				System.out.print("削除する場所 :");
-				//削除する場所を入力する
-				int idx = standardInput.nextInt();
-				//aryRmvメソッドの結果を表示
-				aryRmv(arrayNumber, idx);
-}
+		Scanner standardInput = new Scanner(System.in);
+		//""の中の文字を表示
+		System.out.print("要素数 :");
+		//要素数を入力
+		int inputNumber = standardInput.nextInt();
+		//入力値の分だけ整数を入れられる配列を作る
+		int[] arrayNumber = new int [inputNumber]; 
+		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
+		for(int i = 0; i < inputNumber; i++) {
+			//""の中の文字とiの値を表示
+			System.out.print("arrayNumber [" + i + "} =");
+			//配列[i]の値を入力
+			arrayNumber[i] = standardInput.nextInt();
+		}
+		//""の中の文字を表示
+		System.out.print("{");
+		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
+		for(int i = 0; i < inputNumber - FINAL_VALUE; i++) {
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
+		}
+		//arrayNumber[要素数から-1]の値と""の中の文字を表示して改行
+		System.out.println(arrayNumber[inputNumber - FINAL_VALUE] + "}");
+		//""の中の文字を表示
+		System.out.print("削除する場所 :");
+		//削除する場所を入力する
+		int idx = standardInput.nextInt();
+		//aryRmvメソッドの結果を表示
+		aryRmv(arrayNumber, idx);
+	}
 	/*	
 	 *  関数名 　          :aryRmv
 	 *  メソッドの説明 　　:削除する値より後ろの全要素を1つ前方にずらす、
@@ -69,8 +69,8 @@ public class Question7_18 {
 		System.out.print("{");
 		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
 		for(int i = 0; i < arrayNumber.length - FINAL_VALUE; i++) {
-		//arrayNumber[i]の値と""の中の文字を表示
-		System.out.print(arrayNumber[i] + ", ");	
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
 		}
 		//arrayNumber[要素数から-1]の値と""の中の文字を表示して改行
 		System.out.println(arrayNumber[arrayNumber.length - FINAL_VALUE] + "}");
