8c63d933fe1147ed2eba09cae96a437065b74ff8
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-24 19:34:42 +0900

    owa

---
+++ Question7_23/src/Question7_23/Question7_23.java
@@ -0,0 +1,58 @@
+package Question7_23;
+
+import java.util.Scanner;
+/* 
+ * クラス名 	:Question7_23
+ * 概要 		:配列aと同じ配列を生成して返却する
+ * 作成者 　	:中西輝 
+ * 作成日 		:2026.4.24
+ */
+public class Question7_23 {	
+	/*	
+	 *  関数名 　        :main	
+	 *  メソッドの説明 　:
+	 *  パラメータの説明 :なし	 
+	 *  返り値 　        :なし	 
+	 *  作成者 　        :中西輝	 
+	 *  作成日 　        :2026.4.24	 
+	 */
+
+
+	public static void main(String[] args) {
+		int inputNumber = 0;
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//繰り返し
+		do {
+			//""の中の文字を表示
+			System.out.print("要素数 :");
+			//要素数を入力
+			inputNumber  = standardInput.nextInt();
+			//elementsNumberが0以下の場合繰り返す
+		}while(inputNumber <= 0);
+		//入力値の分だけ整数を入れられる配列を作る
+		int[] arrayNumber = new int [inputNumber]; 
+		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
+		for(int i = 0; i < inputNumber; i++) {
+			//""の中の文字とiの値を表示
+			System.out.print("arrayNumber [" + i + "] =");
+			//配列[i]の値を入力
+			arrayNumber[i] = standardInput.nextInt();
+			
+	}
+		System.out.print("呼び出す値 :");
+		int callNumber  = standardInput.nextInt();
+		
+		arraySrchIdx(arrayNumber,callNumber);
+
+}
+
+	public static void arraySrchIdx(int[] arrayNumber, int callNumber) {
+		System.out.print("{");
+		for(int i = 0; i < arrayNumber.length; i++) {
+			arrayNumber[i] = callNumber;
+		}
+			return arrayNumber;
+		}
+		
+	}
