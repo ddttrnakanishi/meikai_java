f4b0a5640d191d0535975985694e3f89b3885a86
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-24 13:00:13 +0900

    終わりました

---
+++ Question7_19/src/Question7_19/Question7_19.java
@@ -0,0 +1,86 @@
+package Question7_19;
+
+import java.util.Scanner;
+/* 
+ * クラス名 :Question7_19
+ * 概要 :配列arrayNumberから要素arrayNumber[idx]を先頭とするn個の要素を削除する
+ * 作成者 　:中西輝 
+ * 作成日 :2026.4.24
+ */
+public class Question7_19 {	
+	//クラス全体で使える変数FINAL_VALUEに1を代入
+	final static int FINAL_VALUE = 1;
+	/*	
+	 *  関数名 　        :main	
+	 *  メソッドの説明 　:要素数、各要素の値、削除する場所、削除する個数
+	 *  				  を入力しaryRmvNの結果を表示する	
+	 *  パラメータの説明 :なし	 
+	 *  返り値 　        :なし	 
+	 *  作成者 　        :中西輝	 
+	 *  作成日 　        :2026.4.24	 
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//""の中の文字を表示
+		System.out.print("要素数 :");
+		//要素数を入力
+		int inputNumber = standardInput.nextInt();
+		//入力値の分だけ整数を入れられる配列を作る
+		int[] arrayNumber = new int [inputNumber]; 
+		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
+		for(int i = 0; i < inputNumber; i++) {
+			//""の中の文字とiの値を表示
+			System.out.print("arrayNumber [" + i + "] =");
+			//配列[i]の値を入力
+			arrayNumber[i] = standardInput.nextInt();
+		}
+		//""の中の文字を表示
+		System.out.print("削除を始める場所 :");
+		//削除する場所を入力する
+		int idx = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("削除する個数");
+		//削除する個数を入力する
+		int n = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("{");
+		//変数iを0から始めて1ずつ増やしていき入力値-1になるまで繰り返す
+		for(int i = 0; i < inputNumber - FINAL_VALUE; i++) {
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
+		}
+		//arrayNumber[要素数から-1]の値と""の中の文字を表示して改行
+		System.out.println(arrayNumber[inputNumber - FINAL_VALUE] + "}");
+		//aryRmvメソッドの結果を表示
+		aryRmvN(arrayNumber, idx, n);
+	}
+	/*	
+	 *  関数名 　          :aryRmvN
+	 *  メソッドの説明 　　:削除する値より後ろの全要素を入力した個数分前方にずらす、
+	 *  					その結果をもとにずらした結果を表示
+	 *  パラメータの説明　 : 配列、削除する場所、削除する個数
+	 *  返り値 　 　　　　 :なし	 
+	 *  作成者 　          :中西輝	 
+	 *  作成日 　          :2026.4.24	 
+	 */
+	static void aryRmvN(int[]arrayNumber, int idx, int n) {
+		/*変数iを削除を始める値から始めて1ずつ増やしていき要素数から削除する個数を
+		 * 引いた値になるまで繰り返す
+		 */
+		for(int i = idx; i < arrayNumber.length - n; i++) {
+			//arrayNumber[i]にarrayNumber[i+n]した値を代入
+			arrayNumber[i] = arrayNumber[i + n];
+		}
+		//""の中の文字を表示
+		System.out.print("{");
+		//変数iを0から始めて1ずつ増やしていき要素数から-1を引いた値になるまで繰り返す
+		for(int i = 0; i < arrayNumber.length - FINAL_VALUE; i++) {
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
+		}
+		//arrayNumber[要素数-1]の値と""の中の文字を表示して改行
+		System.out.println(arrayNumber[arrayNumber.length - FINAL_VALUE] + "}");
+	}
+
+}
--- 添削用/src/添削用/添削用.java
+++ 添削用/src/添削用/添削用.java
@@ -4,85 +4,55 @@ import java.util.Scanner;
 
 public class 添削用 {
 
-	static void printBits(int inputNumber) {
-		//変数iが31から始めて1ずつ減らしていきiが0まで繰り返す
-		for(int i = 31; i >= 0; i--)
-			//"iビット目が1なら'1'を0なら'0'"を表示する
-			System.out.print(((inputNumber >>> i & 1) == 1)? '1' : '0');
+	static int linearSearchR(int[] arrayNumber, int keyNumber) {
+		//変数iが要素数-1から始めて1ずつ減らしていきiが0になるまでの回数繰り返す
+		for (int i = arrayNumber.length - 1; i >= 0; i--) {
+			//もしキーと一致する要素が見つかった場合
+			if (arrayNumber[i] == keyNumber) {
+				//そのインデックスを返却する
+				return i;
+			}
+		}
+		//見つからなかった場合は-1を返却する
+		return -1;
 	}
-	/*
-	 * 関数名           :rRotate
-	 * メソッドの説明   :整数を右に回転した値を返却する
-	 * パラメータの説明 :回転前の整数、回転数
-	 * 返り値           :右にビット回転した値
-	 * 作成者           :玉澤一輝
-	 * 作成日           :2026.4.23
-	 */
-	public static int rRotate(int originalNumber, int shiftNumber) {
-		//もしshiftNumberが0より小さい場合
-		if(shiftNumber < 0)
-			//-shiftNumber（逆回転、左）にして返却する
-			return lRotate(originalNumber, -shiftNumber);
-				/*元の整数をshiftNumberビット右シフトして0を詰めた値と、
-		 *元の整数を32-shiftNumberビット左シフトして0を詰めた値の論理和を返却する
-		 */
-		return (originalNumber >>> shiftNumber) | (originalNumber << (32 - shiftNumber));
-	}
-	/*
-	 * 関数名           :lRotate
-	 * メソッドの説明   :整数を左に回転した値を返却する
-	 * パラメータの説明 :回転前の整数、回転数
-	 * 返り値           :左にビット回転した値
-	 * 作成者           :玉澤一輝
-	 * 作成日           :2026.4.23
-	 */
-	public static int lRotate(int originalNumber, int shiftNumber) {
-		//もしshiftNumberが0より小さい場合
-		if (shiftNumber < 0) 
-			//-shiftNumber（逆回転、右）にして返却する
-			return rRotate(originalNumber, -shiftNumber);
-		
-		/*元の整数をshiftNumberビット左シフトして0を詰めた値と、
-		 *元の整数を32-shiftNumberビット右シフトして0を詰めた値の論理和を返却する
-		 */
-		return (originalNumber << shiftNumber) | (originalNumber >>> (32 - shiftNumber));
-	}
-	/*
-	 * 関数名           :main
-	 * メソッドの説明   :値を入力し左右に回転させた結果をビット表示する
-	 * パラメータの説明 :なし
-	 * 返り値           :なし
-	 * 作成者           :玉澤一輝
-	 * 作成日           :2026.4.23
+	/**
+	 * 関数名           : main
+	 * メソッドの説明   : 配列の入力を受け取り末尾からキーを探索した結果を表示する
+	 * パラメータの説明 : なし
+	 * 返り値           : なし
+	 * 作成者           : 玉澤一輝
+	 * 作成日           : 2026.4.24
 	 */
 	public static void main(String[] args) {
 		//キーボードで入力した値を入れる変数を作る
 		Scanner standardInput = new Scanner(System.in);
-		//"整数x"を表示する
-		System.out.print("整数x：");
-		//変数integerNumberに入力した値を代入する
-		int integerNumber = standardInput.nextInt();
-		//"回転数n"を表示する
-		System.out.print("回転数n：");
-		//変数rotationNumberに入力した値を代入する
-		int rotationNumber = standardInput.nextInt();
-		//"元のビット数　 ："を表示する
-		System.out.print("元のビット数　 ：");
-		//入力された整数のビット構成を表示する
-		printBits(integerNumber);
-		//改行する
-		System.out.println();
-		//"右にnビット回転："を表示する
-		System.out.print("右にnビット回転：");
-		//整数を回転数nだけ右シフトした結果のビット構成を表示する
-		printBits(rRotate(integerNumber, rotationNumber));
-		//改行する
-		System.out.println();
-		//"左にnビット回転："を表示する
-		System.out.print("左にnビット回転：");
-		//整数を回転数nだけ左シフトした結果のビット構成を表示する
-		printBits(lRotate(integerNumber, rotationNumber));
-		//改行する
-		System.out.println();
+		//"要素数："を表示する
+		System.out.print("要素数：");
+		//変数elementNumberに入力した値を代入する
+		int elementNumber = standardInput.nextInt();
+		//指定された要素数で配列を作成する
+		int[] arrayNumber = new int[elementNumber];
+		//変数iが0から始めて1ずつ増やしていき要素の数繰り返す
+		for (int i = 0; i < elementNumber; i++) {
+			//"arrayNumber[i]：""を表示する
+			System.out.print("arrayNumber[" + i + "]：");
+			//arrayNumber[i]に入力した値を代入する
+			arrayNumber[i] = standardInput.nextInt();
+		}
+		//"探す値："を表示する
+		System.out.print("探す値：");
+		//変数keyNumberに入力した値を代入する
+		int keyNumber = standardInput.nextInt();
+		//linearSearchRメソッドを呼び出し変数resultIndexに末尾からキーを探索した結果を代入する
+		int resultIndex = linearSearchR(arrayNumber, keyNumber);
+		//もしresultIndexが-1の場合
+		if (resultIndex == -1) {
+			//"その値の要素は存在しません。"を表示する
+			System.out.println("その値の要素は存在しません。");
+		} else {
+			//"それはarrayNumber[resultIndex]にあります。"を表示する
+			System.out.println("それはarrayNumber[" + resultIndex + "]にあります。");
+		}
 	}
 }
