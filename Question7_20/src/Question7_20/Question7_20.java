b8503955ac79f20f30a3aa56736006c931ad9f66
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-24 15:50:59 +0900

    終わりました

---
+++ Question7_20/src/Question7_20/Question7_20.java
@@ -0,0 +1,106 @@
+package Question7_20;
+
+import java.util.Scanner;
+/* 
+ * クラス名 :Question7_20
+ * 概要 :配列arrayNumberの要素arrayNumber[idx]に整数を挿入するメソッドを作成
+ * 作成者 　:中西輝 
+ * 作成日 :2026.4.24
+ */
+public class Question7_20 {	
+	//クラス全体で使える変数FINAL_VALUEに1を代入
+	private final static int FINAL_VALUE = 1;
+	private final static int FINAL_VALUETWO = 2;
+	/*	
+	 *  関数名 　        :main	
+	 *  メソッドの説明 　:要素数、各要素の値、整数を挿入する場所、整数
+	 *  				  を入力しaryInsの処理を行う	
+	 *  パラメータの説明 :なし	 
+	 *  返り値 　        :なし	 
+	 *  作成者 　        :中西輝	 
+	 *  作成日 　        :2026.4.24	 
+	 */
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//inputNumberの初期化
+		int inputNumber = 0;
+		//繰り返し
+		do {
+			//""の中の文字を表示
+			System.out.print("要素数 :");
+			//要素数を入力
+			inputNumber  = standardInput.nextInt();
+			//elementsNumberが0以下の場合繰り返す
+		}while(inputNumber <= 0);
+		//入力値の分だけ整数を入れられる配列を作る
+		int[] arrayNumber = new int [inputNumber]; 
+		//変数iを0から始めて1ずつ増やしていき入力値になるまで繰り返す
+		for(int i = 0; i < inputNumber; i++) {
+			//""の中の文字とiの値を表示
+			System.out.print("arrayNumber [" + i + "] =");
+			//配列[i]の値を入力
+			arrayNumber[i] = standardInput.nextInt();
+		}
+		//idxの初期化
+		int idx = 0;
+		//繰り返し
+		do {
+			//""の中の文字を表示
+			System.out.print("整数を挿入する場所 :");
+			//削除する場所を入力する
+			idx   = standardInput.nextInt();
+			//要素値-1の時繰り返す
+		}while(inputNumber - FINAL_VALUE < idx);
+		//""の中の文字を表示
+		System.out.print("整数 :");
+		//整数を入力する
+		int integerNumber = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("{");
+		//変数iを0から始めて1ずつ増やしていき入力値-1になるまで繰り返す
+		for(int i = 0; i < inputNumber - FINAL_VALUE; i++) {
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
+		}
+		//arrayNumber[要素数から-1]の値と""の中の文字を表示して改行
+		System.out.println(arrayNumber[inputNumber - FINAL_VALUE] + "}");
+		//aryRmvメソッドの結果を表示
+		aryIns(arrayNumber, idx, integerNumber);
+	}
+	/*	
+	 *  関数名 　          :aryIns
+	 *  メソッドの説明 　　:配列arrayNumberの要素arrayNumber[idx]に整数を挿入する
+	 *  パラメータの説明　 : 配列、整数を挿入する場所、整数
+	 *  					nullの場合null以外を入力してくださいと表示
+	 *  返り値 　 　　　　 :なし	 
+	 *  作成者 　          :中西輝	 
+	 *  作成日 　          :2026.4.24	 
+	 */
+	private static void aryIns(int[] arrayNumber, int idx, int integerNumber) {
+		//arrayNumberがnullの場合
+		if(arrayNumber == null) {
+			//""の中の文字を表示して改行
+			System.out.println("null以外を入力してください");
+		}
+		//変数iを要素数の値から始めて1ずつ減らしていき挿入する値になるまで繰り返す
+		for(int i = arrayNumber.length; i > idx; i--) {
+			//arrayNumber[i-1]にarrayNumber[i-2]した値を代入
+			arrayNumber[i - FINAL_VALUE] = arrayNumber[i - FINAL_VALUETWO];
+		}
+		//arrayNumber[idx]に整数を代入
+		arrayNumber[idx] = integerNumber;
+
+		//""の中の文字を表示
+		System.out.print("{");
+		//変数iを0から始めて1ずつ増やしていき要素数から-1を引いた値になるまで繰り返す
+		for(int i = 0; i < arrayNumber.length - FINAL_VALUE; i++) {
+			//arrayNumber[i]の値と""の中の文字を表示
+			System.out.print(arrayNumber[i] + ", ");	
+		}
+		//arrayNumber[要素数-1]の値と""の中の文字を表示して改行
+		System.out.println(arrayNumber[arrayNumber.length - FINAL_VALUE] + "}");
+
+	}
+
+}
--- 添削用/src/添削用/添削用.java
+++ 添削用/src/添削用/添削用.java
@@ -4,55 +4,66 @@ import java.util.Scanner;
 
 public class 添削用 {
 
-	static int linearSearchR(int[] arrayNumber, int keyNumber) {
-		//変数iが要素数-1から始めて1ずつ減らしていきiが0になるまでの回数繰り返す
-		for (int i = arrayNumber.length - 1; i >= 0; i--) {
-			//もしキーと一致する要素が見つかった場合
-			if (arrayNumber[i] == keyNumber) {
-				//そのインデックスを返却する
-				return i;
-			}
+	
+	public static int sum0f(int[] a) {
+		//aの値がnullの場合は以下の処理を行う
+		if(a == null) {
+			//noAcceptに-1を代入する。
+			int noAccept = -1;
+			//その値を返却する。
+			return noAccept;
 		}
-		//見つからなかった場合は-1を返却する
-		return -1;
+		//合計値を格納するsumScoreを0で初期化する
+		int sumScore = 0;
+		//iの値がaの構成要素数未満の間、iの値を増加させながら繰り返し実行する。
+		for(int i = 0; i < a.length; i++) {
+			//iの値に応じたインデックスにある要素の値をsumScoreに加算する
+			sumScore += a[i];
+		}
+		//最後に値を返却する。
+		return sumScore;
+
 	}
+
 	/**
-	 * 関数名           : main
-	 * メソッドの説明   : 配列の入力を受け取り末尾からキーを探索した結果を表示する
-	 * パラメータの説明 : なし
-	 * 返り値           : なし
-	 * 作成者           : 玉澤一輝
-	 * 作成日           : 2026.4.24
+	 * 関数名           :main
+	 * メソッドの説明   :最初に処理を行い、他の関数の処理の結果の受け渡しを行う。
+	 * パラメータの説明 :なし
+	 * 返り値           :なし
+	 * 作成者           :川野晟慶
+	 * 作成日           :2026/4/22
 	 */
 	public static void main(String[] args) {
-		//キーボードで入力した値を入れる変数を作る
+		//新しい標準入力ストリームを作成する。
 		Scanner standardInput = new Scanner(System.in);
-		//"要素数："を表示する
-		System.out.print("要素数：");
-		//変数elementNumberに入力した値を代入する
-		int elementNumber = standardInput.nextInt();
-		//指定された要素数で配列を作成する
-		int[] arrayNumber = new int[elementNumber];
-		//変数iが0から始めて1ずつ増やしていき要素の数繰り返す
-		for (int i = 0; i < elementNumber; i++) {
-			//"arrayNumber[i]：""を表示する
-			System.out.print("arrayNumber[" + i + "]：");
-			//arrayNumber[i]に入力した値を代入する
-			arrayNumber[i] = standardInput.nextInt();
+		//scoreRangeを0で初期化する。
+		int scoreRange = 0;
+		//以下の文を実行する。
+		do{
+			//配列の範囲の入力を促す文を表示する。
+			System.out.print("配列の範囲:");	
+			//読み込んだ値でscoreRangeを初期化する。
+			scoreRange = standardInput.nextInt();
 		}
-		//"探す値："を表示する
-		System.out.print("探す値：");
-		//変数keyNumberに入力した値を代入する
-		int keyNumber = standardInput.nextInt();
-		//linearSearchRメソッドを呼び出し変数resultIndexに末尾からキーを探索した結果を代入する
-		int resultIndex = linearSearchR(arrayNumber, keyNumber);
-		//もしresultIndexが-1の場合
-		if (resultIndex == -1) {
-			//"その値の要素は存在しません。"を表示する
-			System.out.println("その値の要素は存在しません。");
-		} else {
-			//"それはarrayNumber[resultIndex]にあります。"を表示する
-			System.out.println("それはarrayNumber[" + resultIndex + "]にあります。");
+		//0未満のあいだはdo文の中をループする。
+		while(scoreRange < 0); 
+		//allRangeは、scoreRangeの範囲の配列を参照する配列変数となる。
+		int[] allRange = new int[scoreRange];
+		//iの値が配列本体の要素数未満のあいだは、値を増加させながら繰り返し実行する。
+		for(int i = 0; i < allRange.length; i++) {
+			//何番目の要素を記入するか示す文を表示する。
+			System.out.print(i + "番目の要素：");
+			//iの値に応じたインデックスに、読み込んだ値を入れて構成要素にする。
+			allRange[i] = standardInput.nextInt();
 		}
+		//全要素を合計した値を表示する
+		System.out.println("全要素の合計は" + sum0f(allRange));
+
+		//nullの処理。
+		sum0f(null);
+
+
 	}
+
+
 }
