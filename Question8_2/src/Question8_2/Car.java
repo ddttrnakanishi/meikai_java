17d05783eedde7d2a076ccf77fb2c247b6c76687
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-29 10:23:39 +0900

    修正しました

--- Question8_2/src/Question8_2/Car.java
+++ Question8_2/src/Question8_2/Car.java
@@ -7,22 +7,48 @@ package Question8_2;
  * 作成日              :2026.4.28
  */
 public class Car {
+	//名前を表すString型のnameを初期化 
+	private String name = null;
+	//幅を表すint型のwidthを初期化 
+	private int width = 0;
+	//高さを表すint型のheightを初期化 
+	private int height = 0;
+	//長さを表すint型のlengthを初期化 
+	private int length = 0;
+	//現在位置のx座標を初期化 
+	private double x = 0;
+	//現在位置のy座標を初期化 
+	private double y = 0;
+	//残り燃料を表すdouble型のfuelを初期化 
+	private double fuel = 0;
 	//何年から乗っているかを表すint型のNewOrOldを初期化
-	private static int NewOrOld = 0;
-	//色を表すString型のcolour
-	private static String colour;
-	//製造年を表すint型のModelYearを初期化
-	private static int ModelYear = 0;
+	private int NewOrOld = 0;
+	//色を表すString型のcolourを初期化 
+	private String colour = null;
+	//製造年を表すint型のModelYearを初期化 
+	private int ModelYear = 0;
 	//ナンバーを表すint型のNumberを初期化
-	private static int Number = 0;
+	private int Number = 0;
 	/*
 	 * コンストラクタ   :Car
-	 * メソッドの説明   :クラスのインスタンスが生成される際のコンストラクタを作成
+	 * コンストラクタの説明   :クラスのインスタンスが生成される際のコンストラクタを作成
 	 * パラメータの説明 :何年から乗っているか、色、製造年、ナンバー
 	 * 作成者           :中西輝
 	 * 作成日           :2026.4.28
 	 */
-	Car(int whatYear, String colour, int ModelYear,int Number){
+	Car(String name,int width,int height,int length,double fuel, int whatYear, String colour, int ModelYear,int Number){
+		//フィールドに仮引数の値を代入
+		this.name = name;
+		//フィールドに仮引数の値を代入
+		this.width = width;
+		//フィールドに仮引数の値を代入
+		this.height = height;
+		//フィールドに仮引数の値を代入
+		this.length = length;
+		//フィールドに仮引数の値を代入
+		this.fuel = fuel;
+		//初期位置を(x,y) = (0.0,0.0)に設定する
+		x = y = 0.0;
 		//フィールドに仮引数の値を代入
 		this.NewOrOld = whatYear;
 		//フィールドに仮引数の文字を代入
@@ -33,6 +59,42 @@ public class Car {
 		this.Number = Number;
 	}
 	/*
+	関数名　　：getX
+	メソッド　：x座標を取得するためのメソッド
+	パラメータ：なし
+	返り値　　：x
+	作成者　　：中西輝
+	日付　　　：2026/04/28
+	 */
+	public double getX() {
+		//xの値を返却
+		return x;
+	}
+	/*
+	関数名　　：getY
+	メソッド　：y座標を取得するためのメソッド
+	パラメータ：なし
+	返り値　　：y
+	作成者　　 :中西輝
+	日付　　　：2026/04/28
+	 */
+	public double getY() {
+		//yの値を返却
+		return y;
+	}
+	/*
+	関数名　　：getFuel
+	メソッド　：残り燃料の数値を取得するためのメソッド
+	パラメータ：なし
+	返り値　　：fuel
+	作成者　　：中西輝
+	日付　　　：2026/04/28
+	 */
+	public double getFuel() {
+		//残り燃料の値を返却
+		return fuel;
+	}
+	/*
 	 * 関数名           :putSpec
 	 * メソッドの説明   :各情報の表示
 	 * パラメータの説明 :なし
@@ -41,12 +103,53 @@ public class Car {
 	 * 作成日           :2026.4.28
 	 */
 	void putSpec() {
+		//""の中の文字と名前の文字を表示して改行		
+		System.out.println("名前：" + name);
+		//""の中の文字と車幅の文字を表示して改行	
+		System.out.println("車幅：" + width + "mm");
+		//""の中の文字と車高幅の文字を表示して改行	
+		System.out.println("車高：" + height + "mm");
+		//""の中の文字と車長の文字を表示して改行	
+		System.out.println("車長：" + length + "mm");
 		//""の中の文字と名前の文字を表示して改行
 		System.out.println("色 :" + colour);
 		//""の中の文字と製造年の値を表示して改行
 		System.out.println("製造年 :" + ModelYear + "年");
 		//""の中の文字とナンバーの値を表示して改行
 		System.out.println("ナンバー :" + Number);
+	}
+	/*
+	関数名　　：move
+	メソッド　：指定された距離だけ車を移動し、残りの燃料と位置を変更するメソッド
+	パラメータ：移動を示す値dx,dy
+	返り値　　：false,true
+	作成者　　：中西輝
+	日付　　　：2026/04/28
+	 */
+	public boolean move(double dx, double dy) {
+		//負の値が入力された場合
+		if (dx < 0 || dy < 0) {
+			//falseを返却
+			return false;
+		}
+		//移動距離の数値を取得し、計算する
+		double dist = Math.sqrt(dx * dx + dy * dy);
+
+		//燃料不足の場合
+		if (dist > fuel) {
+			//falseを返却
+			return false;
+			//それ以外
+		}else {
+			//残り燃料から使った分の燃料を引く
+			fuel -= dist;
+			//xの座標を更新
+			x += dx;
+			//yの座標を更新
+			y += dy;
+			//trueを返却
+			return true;
+		}
 	}
 	/*
 	 * 関数名           :NewAndUsedCar
@@ -57,8 +160,8 @@ public class Car {
 	 * 作成日           :2026.4.28
 	 */
 	public boolean NewAndUsedCar(int whatYear) {
-		//whatYearが5以上の時
-		if(whatYear >= 5) {
+		//whatYearが5より大きい時
+		if(whatYear > 5) {
 			//返却
 			return false;
 		}
--- Question8_2/src/Question8_2/CarTester.java
+++ Question8_2/src/Question8_2/CarTester.java
@@ -2,7 +2,7 @@ package Question8_2;
 
 import java.util.Scanner;
 /*
- * クラス名            :CarTester
+ * クラス名            :CarTester　
  * 概要                :各情報を入力し入力した値をもとにクラスCar型のmyCarを作成し
  * 						putSpecの情報とNewAndUsedCarメソッドの処理の結果を表示
  * 作成者              :中西輝
@@ -12,7 +12,7 @@ public class CarTester {
 	/*
 	 * 関数名           :main
 	 * メソッドの説明   :各情報を入力し入力した値をもとにクラスCar型のmyCarを作成し
-	 * 					putSpecの情報とNewAndUsedCarメソッドの処理の結果を表示
+	 * 					移動距離の計算、putSpecの情報とNewAndUsedCarメソッドの処理の結果を表示
 	 * パラメータの説明 :なし
 	 * 返り値           :なし
 	 * 作成者           :中西輝
@@ -24,9 +24,41 @@ public class CarTester {
 		//""の中の文字を表示して改行
 		System.out.println("車のデータを入力せよ");
 		//""の中の文字を表示
-		System.out.print("何年目？ :");
-		//whatYearの値を入力
-		int whatYear = standardInput.nextInt();
+		System.out.print("車名：");
+		//widthの文字を入力
+		String name = standardInput.next();
+		//""の中の文字を表示
+		System.out.print("車幅：");
+		//widthの値を入力
+		int width = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("高さ：");
+		//heightの値を入力
+		int height = standardInput.nextInt();
+		//""の中の文字を表示
+		System.out.print("長さ：");
+		//lengthの値を入力
+		int length = standardInput.nextInt();
+		//fuelを0で初期化
+		double fuel = 0;
+		//繰り返し
+		do {
+			//""の中の文字を表示
+			System.out.print("ガソリン量：");
+			//fuelの値を入力
+			fuel = standardInput.nextDouble();
+			//入力された値が負の数である場合繰り返す
+		}while(fuel < 0);
+		//whatYearの初期化
+		int whatYear;
+		//繰り返し
+		do {
+			//""の中の文字を表示
+			System.out.print("何年目？ :");
+			//whatYearの値を入力
+			whatYear = standardInput.nextInt();
+			//0未満の時繰り返す
+		}while(whatYear < 0);
 		//""の中の文字を表示
 		System.out.print("色は :");
 		//colourの文字を入力
@@ -40,7 +72,32 @@ public class CarTester {
 		//Numberの値を入力
 		int Number = standardInput.nextInt();
 		//入力した値をもとにクラスCar型のmyCarを作成
-		Car myCar = new Car(whatYear, colour, ModelYear, Number);
+		Car myCar = new Car(name, width,height, length, fuel, whatYear, colour, ModelYear, Number);
+		//trueである限り繰り返す
+		while(true) {
+			//""の中の文字と現在地と残りの年長の値を表示して改行
+			System.out.println("現在地(" + myCar.getX() + ", " + myCar.getY() + ")・残りの燃料 " + myCar.getFuel());
+			//""の中の文字を表示
+			System.out.print("移動しますか[0…No/1…Yes] :");
+			//入力値が0の場合
+			if(standardInput.nextInt() == 0) {
+				//飛び出す
+				break;
+			}
+			//""の中の文字を表示
+			System.out.print("X方向の移動距離 :");
+			//移動距離を入力
+			double dx = standardInput.nextDouble();
+			//""の中の文字を表示
+			System.out.print("Y方向の移動距離 :");
+			//移動距離を表示
+			double dy = standardInput.nextDouble();
+			//falseが返却されたとき
+			if(!myCar.move(dx, dy)) {
+				//""の中の文字を表示して改行
+				System.out.println("燃料が足りません！");
+			}
+		}
 		//myCarの情報をputSpecに沿い表示
 		myCar.putSpec();
 		//myCar.NewAndUsedCar(whatYear)の返却値がfalseの場合
