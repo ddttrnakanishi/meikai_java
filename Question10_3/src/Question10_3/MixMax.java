4c7f2489fa1cbe93ab6755305c5ed73ff9b996b0
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-04-30 19:05:46 +0900

    home

---
+++ Question10_3/src/Question10_3/MixMax.java
@@ -0,0 +1,23 @@
+package Question10_3;
+
+public class MixMax {
+
+public static int minDoubleMin(int x, int y) {
+	return y;
+	
+}
+public static int minTriple(int a, int b, int c) {
+	return c;
+	
+}
+public static int[] minArray(int[] a) {
+	return a;
+	
+}
+
+public static int maxNumber(int a, int b) {
+	return b;
+	
+}
+}
+
---
+++ Question10_3/src/Question10_3/MixMaxTester.java
@@ -0,0 +1,43 @@
+package Question10_3;
+
+import java.util.Scanner;
+
+public class MixMaxTester {
+
+	public static void main(String[] args) {
+		//キーボードで入力した値を入れる変数を作る
+		Scanner standardInput = new Scanner(System.in);
+		//""の中の文字を表示して改行
+		System.out.println("値を入力してください");
+
+		//""の中の文字を表示
+		System.out.print("aの値は :");
+		//inputNumberOneの値を入力
+		int inputNumberOne = standardInput.nextInt();
+
+
+		//""の中の文字を表示
+		System.out.print("bの値は :");
+		//inputNumberTwoの値を入力
+		int inputNumberTwo = standardInput.nextInt();
+
+
+		//""の中の文字を表示
+		System.out.print("cの値は :");
+		//inputNumberThreeの値を入力
+		int inputNumberThree = standardInput.nextInt();
+
+    int elementNumber = 0;
+    int[] arrayNumber = new int[elementNumber];
+    System.out.print("要素数は :");
+    elementNumber = standardInput.nextInt();
+    
+    System.out.println("a,bの最小値は" + );
+    System.out.println("a,b,cの最小値は" + );
+    
+    
+	
+	
+	}
+
+}
