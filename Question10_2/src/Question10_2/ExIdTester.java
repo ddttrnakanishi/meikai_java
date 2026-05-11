b33b236e65e4eadc3a6325bbe0d33ebc058ea1ba
ddttrnakanishi <ddt,tr.nakanishi@gmail.com>
2026-05-11 13:57:04 +0900

    一部修正しました

--- Question10_2/src/Question10_2/ExIdTester.java
+++ Question10_2/src/Question10_2/ExIdTester.java
@@ -19,26 +19,31 @@ public class ExIdTester {
 		ExId numberOne = new ExId();
 		//""の中の文字とnumberOneの返却値を表示して改行
 		System.out.println("numberOneの識別番号 :" + numberOne.getInstanceCounter());
+		//インスタンスを生成
 		ExId.changeNumber();
 		//ExIdクラスの識別番号の作成
 		ExId numberTwo = new ExId();
 		//""の中の文字とnumberTwoの返却値を表示して改行
 		System.out.println("numberTwoの識別番号 :" + numberTwo.getInstanceCounter());
+		//インスタンスを生成
 		ExId.changeNumber();
 		//ExIdクラスの識別番号の作成
 		ExId numberThree = new ExId();
 		//""の中の文字とnumberThreeの返却値を表示して改行
 		System.out.println("numberThreeの識別番号 :" + numberThree.getInstanceCounter());
+		//インスタンスを生成
 		ExId.changeNumber();
 		//ExIdクラスの識別番号の作成
 		ExId numberFour = new ExId();
 		//""の中の文字とnumberFourの返却値を表示して改行
 		System.out.println("numberFourの識別番号 :" + numberFour.getInstanceCounter());
+		//インスタンスを生成
 		ExId.changeNumber();
 		//ExIdクラスの識別番号の作成
 		ExId numberFive = new ExId();
 		//""の中の文字とnumberFiveの返却値を表示して改行
 		System.out.println("numberFiveの識別番号 :" + numberFive.getInstanceCounter());
+		//インスタンスを生成
 		ExId.changeNumber();
 		//ExIdクラスの識別番号の作成
 		ExId numberSix = new ExId();
